d03c524850806609b3d56fa404bbd27cf01858b3
mivekk <lukasz.machutt@gmail.com>
2023-06-17 17:39:46 +0200

    added SEO files

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -13,6 +13,15 @@ export default function App({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1.0, viewport-fit=cover"
         />
+        <meta
+          name="description"
+          content="Introducing 'Liga Liter' - The Discord-inspired Scrabble-Like Online Multiplayer Game! 
+            Engage in thrilling wordplay battles, challenge friends, and showcase your linguistic prowess. 
+            Create game rooms, invite friends, and experience real-time multiplayer matches. 
+            Strategically form words using letter tiles, exploit bonus squares, and dominate the leaderboard.             
+            Liga Liter is the ultimate choice for word enthusiasts and competitive gamers. 
+            Unleash your vocabulary, conquer the realm of words, and become a Liga Liter champion!"
+        />
       </Head>
       <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
         <Component {...pageProps} />
